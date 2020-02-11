ccfc01119410e1cfa24046535a72a1cfba3030ca
Sabal KC <sabalkc462@gmail.com>
2020-02-11 19:12:29 +0545

    Angle calculation inside Joint class

--- pose.py
+++ pose.py
@@ -46,17 +46,33 @@ class Joint:
     y: float
     confidence: float
 
+    # Division by scalar
+    def __truediv__(self, scalar):
+        return Joint(self.x / scalar, self.y / scalar, self.confidence)
+
     # Distance between two joints
     @staticmethod
     def distance(joint1: Joint, joint2: Joint) -> float:
         return np.sqrt(np.square(joint1.x - joint2.x) + np.square(joint1.y - joint2.y))
 
-    # Division by scalar
-    def __truediv__(self, scalar):
-        return Joint(self.x / scalar, self.y / scalar, self.confidence)
+    # Pass 4 joints - joint1, joint2 make one vec and joint3 and joint4 make another vec,
+    # Returns the angle between the two vectors
+    @staticmethod
+    def calculate_angle(joint1: Joint, joint2: Joint, joint3: Joint, joint4: Joint) -> float:
+        vec1 = np.array(Joint.vector_from_joints(joint1, joint2))
+        vec2 = np.array(Joint.vector_from_joints(joint3, joint4))
+
+        # Unit vector conversion
+        vec1 = vec1 / np.linalg.norm(vec1)
+        vec2 = vec2 / np.linalg.norm(vec2)
+
+        # Cos-1 formula for angle
+        angle = np.degrees(
+            np.arccos(np.clip(np.sum(np.multiply(vec1, vec2)), -1.0, 1.0)))
+        return angle
 
     @staticmethod
-    def vectorFromJoints(joint1: Joint, joint2: Joint) -> tuple:
+    def vector_from_joints(joint1: Joint, joint2: Joint) -> tuple:
         return (joint2.x - joint1.x, joint2.y - joint1.y)
 
 
